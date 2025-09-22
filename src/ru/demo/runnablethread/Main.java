d7addb2e1cbe510e71975f51862e5d1ec9f6a3d7
Mary <Anymail@mail.com>
2025-09-22 14:40:51 +0300

    commit

--- src/ru/demo/runnablethread/Main.java
+++ src/ru/demo/runnablethread/Main.java
@@ -1,11 +1,25 @@
-
+package ru.demo.runnablethread;
 public class Main {
     public static void main(String[] args) {
 
+        for (int i = 1; i <= 10; i++) {
+            int h = i;
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 3; j > 0; j--) {
+                        System.out.println("Поток " + h + ": " + j);
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            System.out.println("Поток " + h + " прерван");
+                        }
+                    }
+                    System.out.println("Поток " + h + " завершен");
+                }
+            });
 
+            thread.start();
+        }
     }
-}
-
-public class Thread implements Runnable{
-
 }
